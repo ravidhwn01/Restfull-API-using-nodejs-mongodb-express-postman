490eeb8a26c5c891646d5b7f770982303b3c3f4e
ravi kumar dhawan <ravidhawan8899@gmail.com>
2022-10-13 18:05:38 +0530

    inserted data into database using postman and post req

--- src/app.js
+++ src/app.js
@@ -1,12 +1,22 @@
-const express = require("express")
+const express = require("express");
 require("../src/db/conn");
+const mensRanking = require("../src/models/mens")
 const app = express();
-const PORT =process.env.PORT|| 8000;
-app.get("/",(req,res)=>{
-    res.send("this is home ")
+const PORT = process.env.PORT || 3000;
+app.use(express.json());
+// we will handle post req 
+app.post("/mens", async (req,res)=>{
+    // res.send("this is root of this page!")
+    try {
+        const addingMensRecord = new mensRanking(req.body);
+        console.log(req.body);
+   const insertMens = await  addingMensRecord.save();
+   res.send(insertMens);
+        
+    } catch (e) {
+        res.send(e)
+    }
 })
-
-
-app.listen(PORT,()=>{
-    console.log("connecting is live at port: 8000")
-})
+app.listen(PORT, () => {
+  console.log("connecting is live at port: 3000");
+});
--- src/models/mens.js
+++ src/models/mens.js
@@ -1,10 +1,38 @@
 const express = require("express");
-const mongoose = require("mongoose")
-
+const mongoose = require("mongoose");
+// creating document or schema
 const menSchema = new mongoose.Schema({
-    ranking:{
-        type:Number,
-        required:true,
-        trim:true
-    }
-})
+  ranking: {
+    type: Number,
+    required: true,
+    trim: true,
+  },
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  dob: {
+    type: Date,
+    required: true,
+    trim: true,
+  },
+  Country: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+
+  score: {
+    type: Number,
+    required: true,
+    trim: true,
+  },
+  event: {
+    type: String,
+    default: "100m",
+  },
+});
+// creating a new collection
+const menRanking = new mongoose.model("MenRanking", menSchema);
+module.exports = menRanking;
